5e06a3037373f80faad9b8819a0a8c563111dcc4
Ben Sammons <bsammnz@gmail.com>
2014-09-07 04:01:37 -0500

    registered event model

--- database.js
+++ database.js
@@ -5,27 +5,28 @@ mongoose.connect ('mongodb://localhost/my_database');
 var Schema = mongoose.Schema;
 
 var TestResultSchema = new Schema({
-     testId :  String, // actual type of test
-     error : String, 
-     timeStart : Date,
-     timeEnd : Date,
-     responseTime: Number,
-     response : {
-         code: Number,
-         body: Schema.Types.Mixed,
-         headers: String
-     },
-     serviceName : String,
-     anomaly : String
+ testId :  String, // actual type of test
+ error : String, 
+ timeStart : Date,
+ timeEnd : Date,
+ responseTime: Number,
+ response : {
+   code: Number,
+   body: Schema.Types.Mixed,
+   headers: String
+ },
+ serviceName : String,
+ anomaly : String
 });
 
-var Event = new Schema({
-    description: String,
-    updates: [],
-    resolved: Boolean,
-    createdDate: Date,
-    resolvedDate: Date
+var EventSchema = new Schema({
+  description: String,
+  updates: [],
+  resolved: Boolean,
+  createdDate: Date,
+  resolvedDate: Date
 });
 
 module.exports.TestResult = mongoose.model('TestResult', TestResultSchema);
+module.exports.Event = mongoose.model('Event',EventSchema);
 
