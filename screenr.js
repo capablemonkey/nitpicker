45011d36f30ae79170bba827f3cadd87c8754c17
Ben Sammons <bsammnz@gmail.com>
2014-09-06 23:58:41 -0500

    tracking healthiness over intervals in screenr

--- screenr.js
+++ screenr.js
@@ -7,41 +7,71 @@ function generateNotification(anomaly) {
 
 }
 
+
+
 function flagTestResult(testResult, errorMessage) {
-    database.TestResult.findById(testResult.id, function(err, doc) {
-        if (err !== null) {
-            throw err;
-        } 
-        else if (doc !== null) {
-            doc.anomaly = errorMessage;
-            doc.save(function(err) {
-                throw err;
-            });
-        } else  {
-            throw 'somehow there was not an error, and not a doc from the db';
-        }
-    })
+    
+  database.TestResult.findById(testResult.id, function(err, doc) {
+    if (err !== null) {
+      throw err;
+    } 
+    else if (doc !== null) {
+      doc.anomaly = errorMessage;
+      doc.save(function(err) {
+          throw err;
+      });
+    } else  {
+        throw 'somehow there was not an error, and not a doc from the db';
+    }
+  })
 }
 
+var healthiness = {};
+var healthinessThreshold = 3;
+var periodLength = 5;
+var periodCounter = 0;
 function evaluateTest(testResult) {
     
+  var testRoutine = tests[testResult.serviceName][testResult.id];
+  try {
+      testRoutine.criteria(testResult,function() {
 
-    var testRoutine = tests[testResult.serviceName][testResult.id];
-    try {
-        testRoutine.criteria(testResult,function() {});
+        if (healthiness[testResult.testId] === undefined) {
+          healthiness[testResult.testId] = {
+              periodCounter : 0,
+              score : 0
+          };
+        }
+        
+        var curTestHealthiness = healthiness[testResult.testId];
+        curTestHealthiness.periodCounter++;
+        
+        if ( testResult.responseTime > testRoutine.config.responseTimeThreshold) {
+            curTestHealthiness.score++; // fails one health check
+        }
+        if (curTestHealthiness.score > healthinessThreshold) {
+            // create an event AND flag an anomaly
+            
+            flagTestResult( testResult, "Service Response Time Too Long!" );
+            console.log("Service Response Time Too Long!")
+        }
 
-    } catch (e) {
-        flagTestResult( testResult, e );        
-    }
-
+        if (curTestHealthiness.periodCounter > periodLength) {// interval has ended, reset
+            curTestHealthiness.periodCounter = 0;
+            curTestHealthiness.score = 0;
+        }
+      });
+  } catch (e) {
+      flagTestResult( testResult, e );        
+  }
 
 }
 
 var startScreenr = function(queue) {
-	queue.on('push',function() {
-		var testResult = queue.pop();
-		evaluateTest( testResult );
-	})
+    queue.on('push',function() {
+        var testResult = queue.pop();
+        evaluateTest( testResult );
+    })
 };
 
 module.exports = {
