c7cc44752727327952de917e615e2f1c7c14ff11
Ben Sammons <bsammnz@gmail.com>
2014-09-07 03:47:09 -0500

    Events now being both created and resolved

--- screenr.js
+++ screenr.js
@@ -2,15 +2,16 @@ var tests = require('./tests/tests.js');
 var database = require('./database.js');
 
 
-// interface to contact notifyr
-function generateNotification(anomaly) {
-
-}
-
-
+var unhealthyTests = {};
 
 function flagTestResult(testResult, errorMessage) {
-    console.log('test result flagged!!',errorMessage)
+    //flag
+    if (unhealthyTests[TestResult.testId] === undefined) {
+      createEvent(TestResult, function(doc) {
+        unhealthyTests[testResult.testId] = doc;
+      })
+    }
+    console.log('test result flagged!!',errorMessage);
       testResult.anomaly = errorMessage;
       testResult.save(function(err) { 
         if (err !== null) {
@@ -19,62 +20,73 @@ function flagTestResult(testResult, errorMessage) {
       });
 }
 
-var healthiness = {};
-var healthinessThreshhold = 0.75;// TODO think about the counting method
-var windowWidth = 5;
+function createEvent(testResult) {
+  var testEvent = new database.Event({
+    description: testResult.anomaly,
+    updates: [],
+    resolved: false,
+    createdDate: new Date()
+  });
+  testEvent.save(function(err){
+    if (err !== undefined) throw err;
+  });
+}
+
+function resolveEvent(testResult) {
+  if (unhealthyTests[testResult.testId] === undefined) return;
+  console.log('test result resolved!!');
+  testEvent = unhealthyTests[testResult.testId];
+  testEvent.resolved = true;
+  testEvent.resolvedDate = new Date();
+  delete unhealthyTests[TestResult.testId];
+}
+
+
+
 function evaluateTest(testResult) {
-    
+  var windowWidth = 5;
   var testRoutine = tests[testResult.serviceName][testResult.testId];
 
-  console.log('begin evaluation')
-
   try {
-      testRoutine.criteria(testResult,function() { 
-        database.TestResult
-          .find({'testId': testResult.testId})
-          .limit(windowWidth) /* the size of the interval */
-          .sort('-timeStart')
-          .exec(function(err, docs) {
+    testRoutine.criteria(testResult,function() { 
+      database.TestResult
+      .find({'testId': testResult.testId}) 
+      .limit(windowWidth) // the size of the interval 
+      .sort('-timeStart') // get the latest entries 
+      .exec(function(err, docs) {
+        var averageTime = 0;// Average method
+        if (docs.length < windowWidth) return;
 
-            var numTooSlow = 0;// Counting method
-            var averageTime = 0;// Average method
-            if (docs.length < windowWidth) return;
+        // iterate over rest of documents and find average
+        // also count the number of individual documents aver the threshold
+        for (var i = 0; i < docs.length; i++) {
+          // aggregate time to find average later
+          averageTime += docs[i].responseTime;
+        }
 
-            // iterate over rest of documents and find average
-            // also count the number of individual documents aver the threshold
-            for (var i = 0; i < docs.length; i++) {
-              if (docs[i].responseTime > testRoutine.config.responseTimeThreshold) {
-                numTooSlow++;
-              }
-              // aggregate time to find average later
-              averageTime += docs[i].responseTime;
-            }
+        // determine average
+        averageTime /= docs.length;
 
-            // determine average
-            averageTime /= docs.length;
+        if (averageTime > testRoutine.config.responseTimeThreshold) {
+          throw "Service Response Time Too Long! (Average of last " + docs.length + " exceeded threshold)"
+        } else {
+          resolveEvent(testResult);
+        }
 
-            console.log('average time',averageTime)
-            if (averageTime > testRoutine.config.responseTimeThreshold) {
-              flagTestResult( testResult, "Service Response Time Too Long! (Average of last "+docs.length+" exceeded threshold)" );
-            } 
-            if (numTooSlow > Math.ceil( ( 1 - healthinessThreshhold ) * docs.length ) ) {
-              // if healthiness threshold is .75 then 0.25 of the elements are 
-              // invalid
-              flagTestResult( testResult, "Service Response Time Too Long! (Count of failures in last "+docs.length+" over "+healthinessThreshhold*100+"%)" )
-            }
-          })
-      });
+      })
+    });
   } catch (e) {
-      flagTestResult( testResult, e );        
+    flagTestResult( testResult, e );      
   }
+
 
 }
 
 var startScreenr = function(queue) {
-    queue.on('push',function() {
-        var testResult = queue.pop();
-        evaluateTest( testResult );
-    })
+  queue.on('push',function() {
+    var testResult = queue.pop();
+    evaluateTest( testResult );
+  })
 };
 
 module.exports = {
