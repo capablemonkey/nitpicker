16908d3eebb54df5dd45743aa94a7ce3a278ac91
Gordon Zheng <technix1@gmail.com>
2014-09-06 23:09:04 -0400

    worker now records time, timedelta

--- worker.js
+++ worker.js
@@ -5,26 +5,45 @@ var _ = require('underscore');
 var startWorker = function() {
   _.each(tests, function(service) {
     _.each(service, function(test, name) {
-      
+
       setInterval(function() {
-        runTest(test, name);
+        runTest(test, name, service);
       }, config.testInterval);
 
     });
   });
 };
 
-function runTest(test, name) {
-  console.log(new Date(), 'running test:', name);
+// TODO: run an entire Service every interval secs, which consists of a async.
+
+function runTest(test, name, service) {
+  var beginTime = Date.now();
+  console.log(beginTime, 'running test:', name);
 
   test.execute(function(err, response) {
+    var endTime = Date.now();
+    var responseTime = (endTime - beginTime); // should be in ms
+
     // save err, response in new TestResult object.
     console.log(err);
     console.log(response);
 
     var testResult = {
-      
+      id: null, // database id
+      testId: name,
+      serviceName: service, //production or sandbox
+      error: err,
+      timeStart: Date(beginTime),
+      timeEnd: Date(endTime),
+      responseTime: responseTime,
+      response: {
+        code: null,
+        body: response,
+        headers: null
+      }
     };
+
+    console.log(testResult);
 
   });
 }
