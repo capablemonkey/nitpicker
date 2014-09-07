538f57eed4d9e8c2312569b38ea7958d79f208e0
Gordon Zheng <technix1@gmail.com>
2014-09-07 02:40:45 -0400

    initial frontend dashboard

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	prodOk: true,
+  	prodMessage: 'Smooth sailing!',
+  	sandboxOk: false,
+  	sandboxMessage: 'Experiencing issues...'
+  });
 });
 
 module.exports = router;
