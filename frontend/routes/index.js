6c85bb7b172fb075a358e5874c00f15349899d67
Gordon Zheng <technix1@gmail.com>
2014-09-07 16:07:26 -0400

    show events and test results in front end

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,14 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../../database.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {
-  	prodOk: true,
-  	prodMessage: 'Smooth sailing!',
-  	sandboxOk: false,
-  	sandboxMessage: 'Experiencing issues...'
-  });
+
+	var testResults;
+	var events;
+
+	db.TestResult
+		.find()
+		.sort('-timeStart')
+		.limit(10)
+		.exec()
+
+	.then(function(results){
+		testResults = results;
+		
+		return db.Event
+			.find()
+			.sort('-createdDate')
+			.limit(10)
+			.exec();
+	})
+
+	.then(function(results) {
+		events = results;
+		console.log(events)
+	})
+	.then(function() {
+		res.render('index', {
+	  	prodOk: true,
+	  	prodMessage: 'Smooth sailing!',
+	  	sandboxOk: false,
+	  	sandboxMessage: 'Experiencing issues...',
+	  	events: events,
+	  	results: testResults
+	  });
+	});
+
+  
 });
 
 module.exports = router;
