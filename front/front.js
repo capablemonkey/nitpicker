0fb9d85044fa8af154ef7ce13352c7a435f83cc7
Gordon Zheng <technix1@gmail.com>
2014-10-26 02:31:54 -0400

    refactor; document code

--- front/front.js
+++ front/front.js
@@ -1,17 +1,23 @@
+// Graph Y axis's max and min
 var GRAPH_Y_AXIS_MAX_RESPONSE_TIME = 1500;
 var GRAPH_Y_AXIS_MIN_RESPONSE_TIME = 0;
+
+// conversion of time windows to milliseconds time delta
 var TIME_WINDOW = {
   '1-hour': 1000 * 60 * 60,
   '24-hours': 1000 * 60 * 60 * 24,
   '7-days': 1000 * 60 * 60 * 24 * 7,
   '1-month': 1000 * 60 * 60 * 24 * 7
 };
+
+// session prefixes used to name Session variables
 var SESSION_PREFIX_AVERAGE_RESPONSE_TIME = "avgResponseTime_";
 var SESSION_PREFIX_MAX_RESPONSE_TIME = "maxResponseTime_";
 
 if (Meteor.isClient) {
-  Session.setDefault("timeWindow", '24-hours');
+  Session.setDefault("timeWindow", '1-hour');
 
+  // subscribe to data set published by server
   Meteor.subscribe('testResultz', 100);
   Meteor.subscribe('events', 100);
 
@@ -26,6 +32,8 @@ if (Meteor.isClient) {
 
   Template.endpointsStats.helpers({
     endpoints: function() {
+      // TODO: get list of tests dynamically instead of hardcoding
+
       return [
         {name: 'Basic Account Info'}, 
         {name: 'Balance'},
@@ -69,73 +77,35 @@ if (Meteor.isClient) {
     }
   });
 
-  Tracker.autorun(function() {
-    var latestTestResult = TestResult.findOne({}, { limit: 1, sort: {timeStart: -1}});
-    if (typeof latestTestResult !== 'undefined') {
-      Session.set('lastUpdated', latestTestResult.timeStart);
-    }
-  });
-
+  // Set up graph and response times for endpoint:
   Template.endpointView.rendered = function() {
-
     // TODO: when timeWindow is changed, change the graph data points
 
-    data = [{x: 0, y: 0}, {x: 1, y: 1}];
-
     var endpointName = this.data.endpointName;
-    var DOMName = endpointName.toLowerCase().split(' ').join('-');
-    var template = this;
-
-    var graph = new Rickshaw.Graph( {
-      element: document.querySelector("#graph-" + DOMName),
-      width: 580,
-      height: 250,
-      max: GRAPH_Y_AXIS_MAX_RESPONSE_TIME,
-      min: GRAPH_Y_AXIS_MIN_RESPONSE_TIME,
-      renderer: 'line',
-      series: [ {
-        color: 'steelblue',
-        data: data,
-        name: 'Response Time'
-      } ]
-    } );
-
-    var time = new Rickshaw.Fixtures.Time();
-    var xAxis = new Rickshaw.Graph.Axis.Time({ 
-      graph: graph,
-      timeUnit: time.unit('minute')
-    });
-    var hoverDetail = new Rickshaw.Graph.HoverDetail({ graph: graph });
-    var yAxis = new Rickshaw.Graph.Axis.Y({
-        graph: graph,
-        orientation: 'left',
-        tickFormat: formatResponseTime,
-        element: document.getElementById('y-' + DOMName),
-    });
-
+    
+    // Create new blank graph and render it:
+    var graph = setupGraph(endpointName);
     graph.render();
 
+    // Every time a new test result for this endpoint is added,
+    // update the graph and response times:
     this.autorun(function() {
-      // update graph: 
       results = TestResult.find({testId: endpointName}, {limit: 100});
-      data = [];
-      results.forEach(function(result) {
-        data.push({
+
+      data = results.map(function(result) {
+        return {
           x: result.timeStart.getTime() / 1000,
           y: result.responseTime
-        });
+        };
       });
 
-      data = _.sortBy(data, function(point) { return point.x; });
-
-      console.log('new data,', data);
-
-      graph.series[0].data = data;
+      graph.series[0].data = _.sortBy(data, function(point) { return point.x; });
       graph.render();
 
+      // calulate avg and max response times for the desired time window
       var timeWindowInMS = TIME_WINDOW[Session.get("timeWindow")];
 
-      // update avg response time:
+      // have the server crunch numbers and pass it back to us via Session variable
       Meteor.call('getAverageResponseTime', endpointName, timeWindowInMS, function(err, result) {
         Session.set(SESSION_PREFIX_AVERAGE_RESPONSE_TIME + endpointName, formatResponseTime(result));
       });
@@ -146,8 +116,7 @@ if (Meteor.isClient) {
     });
   };
 
-  Template.endpointView.created = function() {
-  };
+  // Events view Template
 
   Template.nitpickerEvents.helpers({
     getEvents: function() {
@@ -155,11 +124,14 @@ if (Meteor.isClient) {
     }
   });
 
+  // Hello Template
+
   Template.hello.helpers({
     timeWindow: function () {
       return Session.get("timeWindow");
     },
     lastUpdated: function(ISO) {
+      // Show time since last testResult 
       var lastUpdated = Session.get("lastUpdated");
       if (typeof lastUpdated === 'undefined') { return 'Loading...'; }
       if (ISO) { 
@@ -174,16 +146,26 @@ if (Meteor.isClient) {
 
   Template.hello.events({
     'click button': function (event) {
-      // set time window
+      // set desired time window: 1 hour, 24 hours, etc.
       Session.set("timeWindow", event.target.name);
     }
   });
 
   Template.hello.rendered = function() {
+    // set up JQuery timeago to count down time since last testresult
     jQuery.timeago.settings.strings.seconds = '%d seconds';
     jQuery.timeago.settings.refreshMillis = 1000;
     $("time.timeago").timeago();
   };
+
+  // Whenever TestResult collection is updated, pass the time of the latest TestResult
+  // to the hello template via Session variable 'lastUpdated'.
+  Tracker.autorun(function() {
+    var latestTestResult = TestResult.findOne({}, { limit: 1, sort: {timeStart: -1}});
+    if (typeof latestTestResult !== 'undefined') {
+      Session.set('lastUpdated', latestTestResult.timeStart);
+    }
+  });
 
 }
 
@@ -204,6 +186,10 @@ if (Meteor.isServer) {
   Meteor.publish('events', function(limit) {
     return NitpickerEvent.find({}, { limit: limit, sort: {createdDate: -1}});
   });
+
+  /*
+   * Server-side methods:
+   */
 
   Meteor.methods({
     getAverageResponseTime: function(endpointName, timeWindow) {
@@ -234,14 +220,45 @@ if (Meteor.isServer) {
       return result.responseTime;
     }
   });
-
-  // TODO: get list of tests
-  // TODO: for each test, get average response times, testResults for the past hour
 }
 
-// MONGO_URL=mongodb://localhost/my_database meteor --port 8888
-
 // Helper functions:
+
+function setupGraph(endpointName) {
+  var DOMName = endpointName.toLowerCase().split(' ').join('-');
+
+  // bogus data to supply to instantiate graph
+  data = [{x: 0, y: 0}, {x: 1, y: 1}];
+
+  var graph = new Rickshaw.Graph( {
+    element: document.querySelector("#graph-" + DOMName),
+    width: 580,
+    height: 250,
+    max: GRAPH_Y_AXIS_MAX_RESPONSE_TIME,
+    min: GRAPH_Y_AXIS_MIN_RESPONSE_TIME,
+    renderer: 'line',
+    series: [ {
+      color: 'steelblue',
+      data: data,
+      name: 'Response Time'
+    } ]
+  } );
+
+  var time = new Rickshaw.Fixtures.Time();
+  var xAxis = new Rickshaw.Graph.Axis.Time({ 
+    graph: graph,
+    timeUnit: time.unit('minute')
+  });
+  var hoverDetail = new Rickshaw.Graph.HoverDetail({ graph: graph });
+  var yAxis = new Rickshaw.Graph.Axis.Y({
+      graph: graph,
+      orientation: 'left',
+      tickFormat: formatResponseTime,
+      element: document.getElementById('y-' + DOMName),
+  });
+
+  return graph;
+}
 
 var formatResponseTime = function(y) {
   if (y < 1000) { return Math.floor(y) + ' ms'; }
